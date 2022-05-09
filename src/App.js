399680de7690e10fe13023a2affcd46f1579dc3b
Ernesto Quito Gonzales <quito.gonz.ern@gmail.com>
2022-05-09 01:34:38 -0500

    feat: Se agrega bar en la web app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './estilos/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor-app">
+      <nav className='contenedor-menu'>
+        <ul className='contenedor-elementos'>
+          <li className='elemento-menu'><a href='/'>CONFIGURACIÓN</a></li>
+        </ul>
+      </nav>
     </div>
   );
 }
